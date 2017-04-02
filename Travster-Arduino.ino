39e70ca63a02ba67fd4503051c79d77962213861
atikafrds <atikafrds@gmail.com>
2017-04-03 00:37:58 +0700

    Change source code

--- Travster-Arduino.ino
+++ Travster-Arduino.ino
@@ -1,15 +1,27 @@
-#include <LiquidCrystal.h>
 #include <SPI.h>
+#include <LiquidCrystal.h>
 #include "dht.h"
 
-#define DHT11_PIN 10
+#define DHT11_PIN 5
+#define LCD_PIN 10
 #define BUTTON_PIN 12
+#define LED_PIN 9
+#define BUZZER_PIN 8
+#define NOTE_C4  262
+#define NOTE_D4  294
+#define NOTE_E4  330
+#define KEYPAD_PIN A0
 
-const int latchPin = 5;  // Pin connected to Pin 12 of 74HC595 (Latch)
-const int dataPin  = 6;  // Pin connected to Pin 14 of 74HC595 (Data)
-const int clockPin = 7;  // Pin connected to Pin 11 of 74HC595 (Clock)
+const int latchPin = 2;  // Pin connected to Pin 12 of 74HC595 (Latch)
+const int dataPin  = 3;  // Pin connected to Pin 14 of 74HC595 (Data)
+const int clockPin = 4;  // Pin connected to Pin 11 of 74HC595 (Clock)
 
-LiquidCrystal lcd(9);
+int thresholds[16] = {62, 42, 21, 0, 144, 127, 110, 92, 214, 200, 184, 170, 273, 260, 247, 236};
+char keypad[16] = {'1', '2', '3', 'A', '4', '5', '6', 'B', '7', '8', '9', 'C', '*', '0', '#', 'D'}; 
+int melody[] = {NOTE_C4, NOTE_D4, NOTE_E4, 0, NOTE_C4, NOTE_D4, NOTE_E4, 0};
+int noteDurations[] = {4, 4, 4, 4, 4, 4, 4, 4};
+
+LiquidCrystal lcd(LCD_PIN);
 dht DHT;
 int reading;
 int prev = LOW;
@@ -22,21 +34,22 @@ void setup() {
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
   pinMode(BUTTON_PIN, INPUT);
-  pinMode(4, OUTPUT);        //Sets digital pin 13 as output pin
+  pinMode(LED_PIN, OUTPUT);
   int chk = DHT.read11(DHT11_PIN);
   lcd.begin(16, 2);
-  showNumber(counter);
+}
+ 
+void loop() {
+  lcd.setCursor(0, 0);
+  lcd.print(millis()/1000);
+  readButton();
+  receiveData();
+  readHum();
+  showNumber(5);
+  readKeypad();
 }
 
-void loop() {
-//  Read Button
-  reading = digitalRead(BUTTON_PIN);
-  if ((reading == HIGH) && (prev == LOW)) {
-    counter++;
-    showNumber(counter);
-  }
-  prev = reading;
-
+void receiveData() {
   if (Serial.available() > 0) { // Send data only when you receive data:
     data = Serial.read();      //Read the incoming data and store it into variable data
     Serial.print(data);        //Print Value inside data in Serial monitor
@@ -44,15 +57,47 @@ void loop() {
     if (data == '1') {            //Checks whether value of data is equal to 1 
       lcd.setCursor(0, 1);
       lcd.print("Turned on");
-      digitalWrite(4, HIGH);  //If value is 1 then LED turns ON
+      digitalWrite(LED_PIN, HIGH);  //If value is 1 then LED turns ON
     } else if (data == '0') {       //Checks whether value of data is equal to 0
       lcd.setCursor(0, 1);
       lcd.print("Turned off");
-      digitalWrite(4, LOW);   //If value is 0 then LED turns OFF
+      digitalWrite(LED_PIN, LOW);   //If value is 0 then LED turns OFF
     }
   }
+}
 
-  lcd.setCursor(0, 0);
+void readKeypad() {
+  int value = analogRead(KEYPAD_PIN);
+  for (int i = 0; i < 16; i++) {
+    if (abs(value - thresholds[i]) < 5) {
+      Serial.println(keypad[i]);
+      while (analogRead(KEYPAD_PIN) < 1000) {
+        delay(100);
+      }
+    }
+  }
+}
+
+void readButton() {
+  reading = digitalRead(BUTTON_PIN);
+  if ((reading == HIGH) && (prev == LOW)) {
+    playNotes();
+  }
+  prev = reading;
+}
+
+void playNotes() {
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(BUZZER_PIN, melody[thisNote], noteDuration);
+    int pauseBetweenNotes = noteDuration * 1;
+    delay(pauseBetweenNotes);
+    noTone(BUZZER_PIN);
+  }
+}
+
+void readHum() {
+  lcd.setCursor(0, 1);
   lcd.print("Hum = ");
   lcd.print(DHT.humidity);
   lcd.print(" %");
